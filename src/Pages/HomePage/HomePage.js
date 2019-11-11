b449fe77ae24eb50947b1ec23585a813993bd83c
Joon Sik Yang <joonsikyang@gmail.com>
2019-11-11 16:46:13 +0900

    first commit

---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class HomePage extends Component {
+  render() {
+    return <h1>we-marpple</h1>;
+  }
+}
+
+export default HomePage;
---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "../src/Pages/HomePage";
+
+export class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
